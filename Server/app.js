9885234456fbee351acfa77b529448da2c1dcf95
Akanle Jesus <jesuswrites20043@gmail.com>
2023-07-12 18:49:19 +0100

    Update cors app.js

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@
  app.use(express.json());
  app.use[morgan("dev")];
  app.use[cors({ origin: 'https://stale-quiz.onrender.com', credentials: true })];
-  app.use[cors({ origin: true, credentials: true })];
+  // app.use[cors({ origin: true, credentials: true })];
  app.use("/api/users", userRoutes);
  //routes
  const TestRoutes = require("./Routes/test");
